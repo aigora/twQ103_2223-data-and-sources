f81d74ab054d77914673b58b10759913acac797b
mmezquidaf <maria.mezquida@alumnos.upm.es>
2023-04-15 13:02:18 +0200

    Update programa.c
    Banner y librerias

--- programa.c
+++ programa.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <windows.h>
+#include <locale.h>
+#define color SetConsoleTextAttribute
+
+void Banner(){
+	HANDLE hConsole = GetStdHandle (STD_OUTPUT_HANDLE);
+	color (hConsole, 1);
+	
+	printf("      __________     \n"); color(hConsole, 1);
+	printf("     |          |\n"); color(hConsole, 1);
+	printf("     |          |\n"); color(hConsole, 1);
+	printf("     |__________|\n"); color(hConsole, 1);
+	printf("          ||   \n"); color(hConsole, 1);
+	printf("     _____||_____\n"); color(hConsole, 1);
+	printf("    |            |\n"); color(hConsole, 1);
+	printf("    |            |\n"); color(hConsole, 1);
+	printf("    |____________|\n"); color(hConsole, 1);
+	printf("          ||   \n"); color(hConsole, 1);
+	printf("          ||\n"); color (hConsole, 1);
+	printf("   _______||_______\n"); color(hConsole, 1);
+	printf("  |                |\n"); color(hConsole, 1);
+	printf("  |                |\n"); color(hConsole, 1);
+	printf("  |________________|\n"); color(hConsole, 1);
+	
+	color (hConsole, 15);
+	
+	return;
+}
 
 int main () {
-	//Hacer banner FUENTE
+	//Imprimir Banner
+	Banner();
+	
+	//Podemos poner tildes
+	setlocale (LC_CTYPE,"spanish");
 	
 	return 0; 
 }
