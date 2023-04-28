02339f0ab16356c8a73800afcfba0b399717cbc4
CM2113 <c.mmontero@alumnos.upm.es>
2023-04-28 10:33:26 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -60,5 +60,72 @@ int main () {
 	//Podemos poner tildes
 	setlocale (LC_CTYPE,"spanish");
 	
+	do{
+		printf ("\n");
+		printf("	MENÚ DE OPCIONES\n");
+		printf("	Escriba el número que representa la acción que desea realizar:\n");
+    	printf("	1. Instrucciones del programa\n");
+    	printf("	2. Iniciar  sesión\n");
+    	printf("	3. Iniciar programa\n");
+    	printf("	4. Salir\n\n");
+
+		printf("	OPCIÓN: ");
+		scanf("%d", &opcion);
+		
+		switch(opcion){
+			case 1:
+				system("cls");
+				Instrucciones();
+				break;
+				
+			case 2: //Añadir función 
+				system("cls");
+            	printf("\n");
+
+				pfichero=fopen ("Datos.txt", "w");
+
+				printf("	Intruduce tu nombre: \n");
+				printf("	NOMBRE: ");
+				fflush(stdin);
+				gets(Usuario.nombre);
+				printf("\n");
+				
+				printf("	Intruduce tus apellidos: \n");
+				printf("	APELLIDOS: ");
+				fflush(stdin);
+				gets(Usuario.apellidos);
+				printf("\n");
+
+				printf("	Intruduce tu profesión: \n");
+				printf("	PROFESIÓN: ");
+				fflush(stdin);
+				gets(Usuario.profesion);
+				printf("\n");
+
+				printf("	Intruduce tu provincia: \n");
+				printf("	PROVINCIA: ");
+				fflush(stdin);
+				gets(Usuario.provincia);
+				printf("\n");
+
+
+				printf ("	Bienvenido a la aplicación\n\n");
+				printf ("	En breves comenzará el programa\n");
+				printf("\n");
+
+				break;
+				
+			case 3:
+				
+				break;
+		}
+		
+		
+	}while (opcion != 4);
+	
+	
+	system("cls");
+	printf("FIN DEL PROGRAMA\n");
+	
 	return 0; 
 }
