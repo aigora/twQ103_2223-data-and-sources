2d3f8a6b8aa40097a6410df40a8aa7bd53f5cf20
mmezquidaf <maria.mezquida@alumnos.upm.es>
2023-04-28 10:51:40 +0200

    Función inicio de sesión

--- programa.c
+++ programa.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <windows.h>
 #include <locale.h>
+#include <string.h>
 #define color SetConsoleTextAttribute
+#define TAM_MAX 300
 
+
+//Funciones
+
+//Función banner
 void Banner(){
 	HANDLE hConsole = GetStdHandle (STD_OUTPUT_HANDLE);
 	color (hConsole, 1);
@@ -28,32 +34,85 @@ void Banner(){
 	return;
 }
 
+//Función para imprimir datos del fichero (Revisar)
 int ImprimirDatos() {
     int i;
 
     struct TUsuario usuario [TAM_MAX];
 
-    FILE *fprueba;
+    FILE *fdatos;
 
-    fprueba = fopen("inicioprueba.txt", "r");
-    if(fprueba == NULL) {
+    fdatos = fopen("DatosUsuarios.txt", "r");
+    if(fdatos == NULL) {
         printf("Error en la apertura de salida.\n");
         return -1;
     }
 
     i=0;
-    while (fscanf(fprueba, "%s %s %s %s",usuario[i].nombre, usuario[i].apellidos, usuario[i].profesion, usuario[i].provincia) != EOF) {
+    while (fscanf(fdatos, "%s %s %s %s",usuario[i].nombre, usuario[i].apellidos, usuario[i].profesion, usuario[i].provincia) != EOF) {
         printf("%s\t %s\t %s\t %s\n", usuario[i].nombre, usuario[i].apellidos, usuario[i].profesion, usuario[i].provincia);
         i++;
     }
     printf("\n");
-    fclose (fprueba);
+    fclose (fdatos);
 
 
     return 0;
 }
 
+//Función para inicio de sesión
+int InicioDeSesion () {
+	
+	struct TUsuario usuario [TAM_MAX];
+	
+	int i = 0;
+	
+	FILE *fdatos;
+	
+	fdatos = fopen("DatosUsuarios.txt", "a");
+	if(fdatos == NULL) {
+		printf("Error en la apertura de salida.\n");
+		return -1;
+	}
+	
+	printf("	Intruduce tu nombre: \n");
+	printf("	NOMBRE: ");
+	fflush(stdin);
+	gets(usuario[i].nombre);
+	printf("\n");
+	fprintf(fdatos, "%s\t", usuario[i].nombre);
+				
+	printf("	Intruduce tus apellidos: \n");
+	printf("	APELLIDOS: ");
+	fflush(stdin);
+	gets(usuario[i].apellidos);
+	printf("\n");
+	fprintf(fdatos, "%s\t", usuario[i].apellidos);
+	
+	printf("	Intruduce tu profesión: \n");
+	printf("	PROFESIÓN: ");
+	fflush(stdin);
+	gets(usuario[i].profesion);
+	printf("\n");
+	fprintf(fdatos, "%s\t", usuario[i].profesion);
+
+	printf("	Intruduce tu provincia: \n");
+	printf("	PROVINCIA: ");
+	fflush(stdin);
+	gets(usuario[i].provincia);
+	printf("\n");
+	fprintf(fdatos, "%s\t", usuario[i].provincia);
+	fprintf(fdatos, "\n");
+	
+	return 0;
+}
+
+//Función verificación de datos
+
 int main () {
+	//Declaración de variables
+	int i;
+	FILE *fdatos;	
 	//Imprimir Banner
 	Banner();
 	
@@ -64,10 +123,11 @@ int main () {
 		printf ("\n");
 		printf("	MENÚ DE OPCIONES\n");
 		printf("	Escriba el número que representa la acción que desea realizar:\n");
-    	printf("	1. Instrucciones del programa\n");
-    	printf("	2. Iniciar  sesión\n");
-    	printf("	3. Iniciar programa\n");
-    	printf("	4. Salir\n\n");
+    	printf("	1. Instrucciones del programa.\n");
+    	printf("	2. Iniciar  sesión.\n");
+    	printf("	3. Verificación de datos.\n");
+    	printf("	4. Iniciar programa.\n");
+    	printf("	5. Salir del programa.\n");
 
 		printf("	OPCIÓN: ");
 		scanf("%d", &opcion);
@@ -81,47 +141,22 @@ int main () {
 			case 2: //Añadir función 
 				system("cls");
             	printf("\n");
-
-				pfichero=fopen ("Datos.txt", "w");
-
-				printf("	Intruduce tu nombre: \n");
-				printf("	NOMBRE: ");
-				fflush(stdin);
-				gets(Usuario.nombre);
-				printf("\n");
-				
-				printf("	Intruduce tus apellidos: \n");
-				printf("	APELLIDOS: ");
-				fflush(stdin);
-				gets(Usuario.apellidos);
-				printf("\n");
-
-				printf("	Intruduce tu profesión: \n");
-				printf("	PROFESIÓN: ");
-				fflush(stdin);
-				gets(Usuario.profesion);
-				printf("\n");
-
-				printf("	Intruduce tu provincia: \n");
-				printf("	PROVINCIA: ");
-				fflush(stdin);
-				gets(Usuario.provincia);
-				printf("\n");
-
-
-				printf ("	Bienvenido a la aplicación\n\n");
-				printf ("	En breves comenzará el programa\n");
-				printf("\n");
-
+            	InicioDeSesion ();
 				break;
 				
 			case 3:
+				system("cls");
+            	printf("\n");
+				ImprimirDatos();
+				break;
+			
+			case 4:
 				
 				break;
 		}
 		
 		
-	}while (opcion != 4);
+	}while (opcion != 5);
 	
 	
 	system("cls");
