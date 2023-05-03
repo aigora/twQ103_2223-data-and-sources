3d9381bad4287a7e1618c7b5b9278c9f0cdc3652
Teresa-CorralesTorresano <101127695+Teresa-CorralesTorresano@users.noreply.github.com>
2023-05-03 10:47:27 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -161,7 +161,7 @@ int ImprimirFicheros (){
 		printf("%s", linea);
 	
    		while (fscanf(flavapies, "%s %f %d %d %d", fuentes[num_fuentes].fuentes, &fuentes[num_fuentes].pH, &fuentes[num_fuentes].conductividad, &fuentes[num_fuentes].turbidez, &fuentes[num_fuentes].coliformes) != EOF) {
-	  		 printf("%s\t %.2f\t %d\t %d\t %d\n", fuentes[num_fuentes].fuentes, fuentes[num_fuentes].pH, fuentes[num_fuentes].conductividad, fuentes[num_fuentes].turbidez, fuentes[num_fuentes].coliformes);
+	  		 printf("%s\t\t %.2f\t\t\t %d\t\t %d\t\t %d\n", fuentes[num_fuentes].fuentes, fuentes[num_fuentes].pH, fuentes[num_fuentes].conductividad, fuentes[num_fuentes].turbidez, fuentes[num_fuentes].coliformes);
 	  		 if (++num_fuentes >= TAM_MAX){
         		printf("Error: demasiadas fuentes en el fichero\n");
         		break;
